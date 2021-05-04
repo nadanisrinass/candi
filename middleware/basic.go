0fdc2e27f70d3dee129d4849297ac7449fb5709b
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2021-05-05 05:23:59 +0700

    rename package name

--- middleware/basic.go
+++ middleware/basic.go
@@ -7,14 +7,14 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/golangid/candi/candishared"
+	"github.com/golangid/candi/config/env"
+	"github.com/golangid/candi/tracer"
+	"github.com/golangid/candi/wrapper"
 	gqlerr "github.com/golangid/graphql-go/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"pkg.agungdp.dev/candi/candishared"
-	"pkg.agungdp.dev/candi/config/env"
-	"pkg.agungdp.dev/candi/tracer"
-	"pkg.agungdp.dev/candi/wrapper"
 )
 
 const (
