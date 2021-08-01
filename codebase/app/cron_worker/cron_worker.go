ad9a185b9a04dbc6b7312a0e386e78727df5dc77
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2021-08-01 15:14:32 +0700

    rename package name

--- codebase/app/cron_worker/cron_worker.go
+++ codebase/app/cron_worker/cron_worker.go
@@ -11,13 +11,13 @@ import (
 	"sync"
 	"time"
 
-	"pkg.agungdp.dev/candi/candihelper"
-	"pkg.agungdp.dev/candi/candiutils"
-	"pkg.agungdp.dev/candi/codebase/factory"
-	"pkg.agungdp.dev/candi/codebase/factory/types"
-	"pkg.agungdp.dev/candi/config/env"
-	"pkg.agungdp.dev/candi/logger"
-	"pkg.agungdp.dev/candi/tracer"
+	"github.com/golangid/candi/candihelper"
+	"github.com/golangid/candi/candiutils"
+	"github.com/golangid/candi/codebase/factory"
+	"github.com/golangid/candi/codebase/factory/types"
+	"github.com/golangid/candi/config/env"
+	"github.com/golangid/candi/logger"
+	"github.com/golangid/candi/tracer"
 )
 
 type cronWorker struct {
